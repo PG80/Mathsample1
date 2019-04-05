a8dd7aeb7de89368763ba48b5556c54f5782941f
PG <47541034+PG80@users.noreply.github.com>
2019-04-05 19:23:38 +0300

    Add files via upload

--- 01_PG80 Math_program_learning_question_answer.py
+++ 01_PG80 Math_program_learning_question_answer.py
@@ -6,6 +6,9 @@
 # Standart_jump
 # Hardcore_jump
 # Professional_jump
+
+import random
+
 from PG80_Math_program_Data_info import Beginner_jump, Easy_jump, Standart_jump, Hardcore_jump, Professional_jump
 
 class basic_logic_for_first_app:
@@ -38,7 +41,23 @@ for serial_number, number01, sign, number02, equals_sign, answer in Beginner_jum
 
 
     if current_question.check_answer(input('What answer?')):
-        print('Good job')
+        first_is_correct_some_good_worlds = [
+        'Unbelievable!',
+        'Super!',
+        'It’s impossible!',
+        'It can’t be true!',
+        'I can’t believe it!',
+        'Incredible!',
+        'I’m speechless!',
+        'How amazing!',
+        'I am astounded!',
+        'I am shocked!',
+        'That’s a good one!',
+        'I can’t imagine you did that!',
+        'I’ll believe it when pigs fly!',
+        ]
+        print(random.choice(first_is_correct_some_good_worlds))
+
 
     elif current_question.check_answer(input('Try, one more')):
         print('Excellent, Street forward!')
@@ -48,6 +67,16 @@ for serial_number, number01, sign, number02, equals_sign, answer in Beginner_jum
 
     else:
         print(number01, sign, number02, equals_sign, answer)
+
+        a = [
+            'У тебя все получится!',
+            'Ты на правильном пути!',
+            'Не расстраивайся, все впереди!',
+            'Посмотри внимательно на правильный ответ и продолжай!',
+            'Запоминай выражения все отлично!',
+        ]
+        print(random.choice(a))
+
 
 
 
